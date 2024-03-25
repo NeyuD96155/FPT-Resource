2a9a105c58f94b46477e225cfdb311ca6b827bd0
https://github.com/NeyuD96155 <phuoc10a12az@gmail.com>
2024-03-25 19:46:28 +0700

    ver 1.01

---
+++ Resource/spring24/FER301/L3/ListOfPlayers.js
@@ -0,0 +1,8 @@
+export const Players =[
+    {id: 1, name: 'Cristiano Ronaldo', club: 'Manchester United', img:'assets/images/cr.jpg'},
+    {id: 2, name: 'Kante', club: 'Chelsea', img:'assets/images/kante.jpg'},
+    {id: 3, name: 'Messi', club: 'PSG', img:'assets/images/messi.jpg'},
+    {id: 4, name: 'Neymar', club: 'PSG',img:'assets/images/neymar.jpg'},
+    {id: 5, name: 'Kane', club: 'Tottemham',img:'assets/images/kane.jpg' },
+    {id: 6, name: 'Haaland', club: 'Manchester City', img:'assets/images/haaland.jpg'}
+  ];
